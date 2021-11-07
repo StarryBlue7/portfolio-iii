09883bde81700ff6c2f68ca2f3354e02a488b7d4
Vince <starryblue7@gmail.com>
2021-11-07 13:02:01 -0800

    add helpers & export models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require("./User");
+const Project = require("./Project");
+const Skill = require("./Skill");
+const Employment = require("./Employment");
+
+module.exports = {
+    User,
+    Project,
+    Skill,
+    Employment
+  };
