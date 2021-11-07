715779ee94ef701e013a1839a0253964def0248d
Vince <starryblue7@gmail.com>
2021-11-07 13:25:59 -0800

    remove user & authentications

--- models/index.js
+++ models/index.js
@@ -1,10 +1,8 @@
-const User = require("./User");
 const Project = require("./Project");
 const Skill = require("./Skill");
 const Employment = require("./Employment");
 
 module.exports = {
-    User,
     Project,
     Skill,
     Employment
