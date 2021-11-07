66d3486fddb6cc34b9e58ecbc4c7dbe3478519b3
Vince <starryblue7@gmail.com>
2021-11-07 14:26:47 -0800

    remove handlebars & helpers

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
 
 const sequelize = require('./config/connection');
@@ -7,12 +6,6 @@ const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
