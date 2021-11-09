d5852e161d80395670e3170f8d5d6cfa49c9d8f4
Vince <starryblue7@gmail.com>
2021-11-08 19:34:34 -0800

    add skill data get request

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,5 @@
 // Sticky navbar behavior
 const offset = $('#navbar').offset();
-
 window.onscroll = function() {
     if (window.pageYOffset >= offset.top) {
         $('#navbar').addClass('sticky-nav');
@@ -9,15 +8,26 @@ window.onscroll = function() {
     }
 }
 
+// Get & render skills data
+$.ajax({
+    url: 'https://immense-lake-41636.herokuapp.com/api/skills',
+    method: 'GET',
+  }).then(function (response) {
+    let skillList = "";
+    response.forEach(skill => {
+        skillList =+ `<li>${skill.name}</li>`
+    });
+    $('#skill-list').html(skillList);
+});
+
 // Video background script
 const frameNumber = 0;
 const playbackConst = 1000;       
 const vid = document.getElementById('v0'); 
-
 function scrollPlay(){  
-  let frameNumber = window.pageYOffset/playbackConst;
-  vid.currentTime = frameNumber;
-  window.requestAnimationFrame(scrollPlay);
+    let frameNumber = window.pageYOffset/playbackConst;
+    vid.currentTime = frameNumber;
+    window.requestAnimationFrame(scrollPlay);
 }
 
 window.requestAnimationFrame(scrollPlay);
