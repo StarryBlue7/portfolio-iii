6b938c8c70b21cde55133fc0cf2d77205041f2f3
Vince <starryblue7@gmail.com>
2021-11-08 18:46:37 -0800

    add api get routes behavior

--- controllers/api.js
+++ controllers/api.js
@@ -6,17 +6,38 @@ const { Project, Skill, Employment } = require('../models');
 
 // Get projects
 router.get('/projects', async (req, res) => {
-    res.json('Projects data')
+    try {
+        const projectData = await Project.findAll({});
+        const projects = projectData.map((project) => project.get({ plain: true }));
+
+        res.status(200).json(projects);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // Get skills
 router.get('/skills', async (req, res) => {
-    res.json('Skills data')
+    try {
+        const skillData = await Skill.findAll({});
+        const skills = skillData.map((skill) => skill.get({ plain: true }));
+
+        res.status(200).json(skills);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // Get employments
 router.get('/employments', async (req, res) => {
-    res.json('Employment data')
+    try {
+        const employmentData = await Employment.findAll({});
+        const employments = employmentData.map((employment) => employment.get({ plain: true }));
+
+        res.status(200).json(employments);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
